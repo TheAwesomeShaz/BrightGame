e95abe251f8f851d9905e21354e97576953dde6f
Knightmare-sama <51862748+Knightmare-sama@users.noreply.github.com>
2020-09-13 02:26:38 +0530

    added Floating Joystick

---
+++ BrightGame/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = target.transform.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPos;
+
+        transform.LookAt(target.transform);
+    }
+}
---
+++ BrightGame/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator anim;
+    public FloatingJoystick joystick;
+    public float moveMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(joystick.Horizontal * moveMultiplier,
+            transform.position.y,transform.position.z);
+    }
+    
+
+}
